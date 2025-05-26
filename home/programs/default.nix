2a0a3253e77cd911b97ff9c0f6403a9e0c3db8f8
JiMarchel <jmarchel100@gmail.com>
2025-05-26 13:34:02 +0700

    nixvim

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,6 +1,7 @@
 {
 	imports = [
 		./git.nix
+		./nixvim.nix
 		./packages.nix
 	];
 }
--- home/programs/packages.nix
+++ home/programs/packages.nix
@@ -4,7 +4,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    neovim
     font-awesome
   ];
 
