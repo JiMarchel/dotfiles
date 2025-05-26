6f7b561677d1e066f5aee3b99edf409f012e159c
JiMarchel <jmarchel100@gmail.com>
2025-05-26 14:54:53 +0700

    nvf lsp dll

--- home/programs/nvf.nix
+++ home/programs/nvf.nix
@@ -15,6 +15,43 @@ programs.nvf = {
         shiftwidth = 2;
         wrap = false;
       };
+
+      clipboard = {
+        enable = true;
+        registers = "unnamedplus";
+        providers = {
+          wl-copy.enable = true;
+          xsel.enable = true;
+        };
+      };
+telescope.enable = true;
+languages = {
+        enableLSP = true;
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+
+        nix.enable = true;
+        clang.enable = true;
+        zig.enable = true;
+        python.enable = true;
+        markdown.enable = true;
+        ts = {
+          enable = true;
+          lsp.enable = true;
+          format.type = "prettierd";
+          extensions.ts-error-translator.enable = true;
+        };
+        html.enable = true;
+        lua.enable = true;
+        css.enable = true;
+        typst.enable = true;
+        rust = {
+          enable = true;
+          crates.enable = true;
+        };
+      };
+
 };
 };
 }
